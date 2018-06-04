46e7adf35e6ccf9ac84b61fb793dcdf0645b74f1
kolliparacs <subhash.kollipara@gmail.com>
2018-06-04 15:19:24 -0500

    needs work
    move all common imports out of this file

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,33 +1,39 @@
 /* tslint:disable:no-unused-variable */
+require('core-js/es7/reflect');
+import 'zone.js/dist/zone.js';
+import 'zone.js/dist/long-stack-trace-zone';
+import 'zone.js/dist/proxy.js';
+import 'zone.js/dist/sync-test';
+import 'zone.js/dist/jasmine-patch';
+import 'zone.js/dist/async-test';
+import 'zone.js/dist/fake-async-test';
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app';
-
+import { By }              from '@angular/platform-browser';
+import { DebugElement }    from '@angular/core';
 describe('AppComponent', () => {
-  beforeEach(() => {
+  beforeEach((done) => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
-      ],
-    });
-    TestBed.compileComponents();
+      ]
+    }).compileComponents().then(done);
   });
-
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+  it('should be truthy',async(()=> {
+    let fixture = TestBed.createComponent(AppComponent);
+    let app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+  it('should be title',()=> {
+    let fixture = TestBed.createComponent(AppComponent);
+    let app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
+  });
+  it('should be title',()=> {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  }));
+  });
+
 });
